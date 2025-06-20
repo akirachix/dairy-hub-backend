93d4e5335eadca5bd48fc7ad3ec951ca2740bcda
habtamneshmarye <habtamneshmarye@gmail.com>
2025-06-20 16:41:04 +0300

    created products

--- Dairy_Hub/Dairy_Hub/settings.py
+++ Dairy_Hub/Dairy_Hub/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'suppliers',
     'farmers',
+    'products',
 ]
 
 MIDDLEWARE = [
---
+++ Dairy_Hub/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    product_image_url = models.ImageField(upload_to='products/', null=True, blank=True)
+    def __str__(self):
+        return self.name
