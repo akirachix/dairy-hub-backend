4a9e75356cdb8183714f134acb16454b6deed39e
habtamneshmarye <habtamneshmarye@gmail.com>
2025-06-23 16:24:31 +0300

    added supplier model

--- Dairy_Hub/suppliers/admin.py
+++ Dairy_Hub/suppliers/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Supplier
 
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_email', 'phone_number', 'created_at')
+    def get_email(self, obj):
+        return obj.user.email
 
-admin.site.register(Supplier)
+    get_email.short_description = 'Email'
+    
+admin.site.register(Supplier, SupplierAdmin)
