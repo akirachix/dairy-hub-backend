53254cb22ca7dc7b4255dee067ecfc144d26641b
Mercy Mwikali <mwikalimercykilonzo.com>
2025-06-24 12:07:29 +0300

    created the api folder

--- Dairy_Hub/suppliers/admin.py
+++ Dairy_Hub/suppliers/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Supplier
+from .models import Supplier 
 
-class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('user', 'get_email', 'phone_number', 'created_at')
-    def get_email(self, obj):
-        return obj.user.email
-
-    get_email.short_description = 'Email'
-    
-
-from .models import Supplier
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ('user', 'get_email', 'phone_number', 'created_at')
     def get_email(self, obj):
