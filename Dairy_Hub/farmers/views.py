129957c00b15a20c240d29c9f63a382592e2da99
Mercy Mwikali <mwikalimercykilonzo.com>
2025-06-25 12:56:36 +0300

    deleted serializer

--- Dairy_Hub/farmers/views.py
+++ Dairy_Hub/farmers/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
-
-from farmers.models import Farmer
-
-from .serializers import FarmerSerializer
-
-class FarmerViewSet(viewsets.ModelViewSet):
-     queryset = Farmer.objects.all()
-
-     serializer_class = FarmerSerializer
